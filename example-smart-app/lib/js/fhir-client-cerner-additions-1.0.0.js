eca85e9afe318ddaeb94de7ac38a2f4d210144b2
Schwirtlich <schwirtt@emea.roche.com>
2020-11-17 11:54:02 +0100

    fix: apply prettier

--- example-smart-app/lib/js/fhir-client-cerner-additions-1.0.0.js
+++ example-smart-app/lib/js/fhir-client-cerner-additions-1.0.0.js
@@ -1,4 +1,4 @@
-/******/ (function(modules) { // webpackBootstrap
+/******/ (function (modules) { // webpackBootstrap
 /******/  // The module cache
 /******/  var installedModules = {};
 /******/
@@ -6,7 +6,7 @@
 /******/  function __webpack_require__(moduleId) {
 /******/
 /******/    // Check if module is in cache
-/******/    if(installedModules[moduleId])
+/******/    if (installedModules[moduleId])
 /******/      return installedModules[moduleId].exports;
 /******/
 /******/    // Create a new module (and put it into the cache)
@@ -14,7 +14,8 @@
 /******/      exports: {},
 /******/      id: moduleId,
 /******/      loaded: false
-/******/    };
+      /******/
+};
 /******/
 /******/    // Execute the module function
 /******/    modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
@@ -24,7 +25,8 @@
 /******/
 /******/    // Return the exports of the module
 /******/    return module.exports;
-/******/  }
+    /******/
+}
 /******/
 /******/
 /******/  // expose the modules object (__webpack_modules__)
@@ -38,115 +40,121 @@
 /******/
 /******/  // Load entry module and return exports
 /******/  return __webpack_require__(0);
-/******/ })
+  /******/
+})
 /************************************************************************/
-/******/ ([
+/******/([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function (module, exports, __webpack_require__) {
 
-  module.exports = __webpack_require__(1);
+    module.exports = __webpack_require__(1);
 
 
-/***/ }),
+    /***/
+}),
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function (module, exports, __webpack_require__) {
 
-  // This is the entry point for webpack to distribute assets
+    // This is the entry point for webpack to distribute assets
 
-  // Require all JS and output as a single js bundle.
-  // Note, we are requiring the es6 js.
-  __webpack_require__(2);
+    // Require all JS and output as a single js bundle.
+    // Note, we are requiring the es6 js.
+    __webpack_require__(2);
 
 
-/***/ }),
+    /***/
+}),
 /* 2 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function (module, exports, __webpack_require__) {
 
-  'use strict';
+    'use strict';
 
-  var _fhirClientCernerAdditions = __webpack_require__(3);
+    var _fhirClientCernerAdditions = __webpack_require__(3);
 
-  var _fhirClientCernerAdditions2 = _interopRequireDefault(_fhirClientCernerAdditions);
+    var _fhirClientCernerAdditions2 = _interopRequireDefault(_fhirClientCernerAdditions);
 
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-  /**
-  * Prereq: This library should be included after fhir-client.js library.
-  *
-  * Automatically sets the fullSessionStorageSupport flag
-  * when this library is included after fhir-client.js.
-  * The boolean value is computed based on whether the page
-  * is rendered within PowerChart or elsewhere.
-  *
-  * If the page is rendered within PowerChart, the value will
-  * be set to false.  This is done to prevent session data
-  * to be shared between applications. The reason is the embedded
-  * IE browser is instantiated on a single thread in PowerChart.
-  */
-  _fhirClientCernerAdditions2.default.setFullSessionStorageSupport();
+    /**
+    * Prereq: This library should be included after fhir-client.js library.
+    *
+    * Automatically sets the fullSessionStorageSupport flag
+    * when this library is included after fhir-client.js.
+    * The boolean value is computed based on whether the page
+    * is rendered within PowerChart or elsewhere.
+    *
+    * If the page is rendered within PowerChart, the value will
+    * be set to false.  This is done to prevent session data
+    * to be shared between applications. The reason is the embedded
+    * IE browser is instantiated on a single thread in PowerChart.
+    */
+    _fhirClientCernerAdditions2.default.setFullSessionStorageSupport();
 
-/***/ }),
+    /***/
+}),
 /* 3 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function (module, exports, __webpack_require__) {
 
-  'use strict';
+    'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
 
-  var _utils = __webpack_require__(4);
+    var _utils = __webpack_require__(4);
 
-  var _utils2 = _interopRequireDefault(_utils);
+    var _utils2 = _interopRequireDefault(_utils);
 
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-  /**
-  * Wrapper object containing additional feature or settings
-  * to the open source version of fhir-client.js file.
-  */
-  var FhirClientCernerAdditions = {
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
     /**
-    * Override fullSessionStorageSupport flag in fhir-client.js (src/client/bb-client.js) file.
-    * The value depends on whether the page is loaded in PowerChart or
-    * a regular browser that supports sessionStorage API without session being shared across tabs.
+    * Wrapper object containing additional feature or settings
+    * to the open source version of fhir-client.js file.
     */
-    setFullSessionStorageSupport: function setFullSessionStorageSupport() {
-      FHIR.oauth2.settings.fullSessionStorageSupport = !_utils2.default.isPowerChart();
-    }
-  }; /* globals FHIR */
-  exports.default = FhirClientCernerAdditions;
+    var FhirClientCernerAdditions = {
 
-/***/ }),
+      /**
+      * Override fullSessionStorageSupport flag in fhir-client.js (src/client/bb-client.js) file.
+      * The value depends on whether the page is loaded in PowerChart or
+      * a regular browser that supports sessionStorage API without session being shared across tabs.
+      */
+      setFullSessionStorageSupport: function setFullSessionStorageSupport() {
+        FHIR.oauth2.settings.fullSessionStorageSupport = !_utils2.default.isPowerChart();
+      }
+    }; /* globals FHIR */
+    exports.default = FhirClientCernerAdditions;
+
+    /***/
+}),
 /* 4 */
-/***/ (function(module, exports) {
+/***/ (function (module, exports) {
 
-  'use strict';
+    'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-  /* globals window */
-
-  /**
-  * This Utils object contains util function(s) needed by this project.
-  * All the functions in this file can be moved to fhir-client-cerner-additions.js file,
-  * however having them here make it easier to write tests.
-  */
-  var Utils = {
+    Object.defineProperty(exports, "__esModule", {
+      value: true
+    });
+    /* globals window */
 
     /**
-    * Determine if application is rendered in PowerChart.
-    * @return null or true/false
+    * This Utils object contains util function(s) needed by this project.
+    * All the functions in this file can be moved to fhir-client-cerner-additions.js file,
+    * however having them here make it easier to write tests.
     */
-    isPowerChart: function isPowerChart() {
-      return window.external && typeof window.external.DiscernObjectFactory !== 'undefined';
-    }
-  };
+    var Utils = {
 
-  exports.default = Utils;
+      /**
+      * Determine if application is rendered in PowerChart.
+      * @return null or true/false
+      */
+      isPowerChart: function isPowerChart() {
+        return window.external && typeof window.external.DiscernObjectFactory !== 'undefined';
+      }
+    };
 
-/***/ })
-/******/ ]);
+    exports.default = Utils;
+
+    /***/
+})
+/******/]);
 //# sourceMappingURL=fhir-client-cerner-additions-1.0.0.min.js.map
